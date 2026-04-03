b30182fe0bcc455f734b92494c66a593a7a34606
Prince <masuke.official@gmail.com>
2026-04-03 20:10:54 +0700

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes,  Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Landingpage from './Pages/Landingpage'
 import Notfound from './Pages/NotFound'
 import UserInterface from './Pages/UserInterface'
@@ -12,8 +12,11 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Landingpage/>}/>
+      <Route path='/login' element={<Landingpage/>}/>
       <Route path='/admin' element={<Admin/>}/>
+      <Route path='/Admin' element={<Navigate to="/admin" replace />} />
       <Route path='/user' element={<UserInterface/>}/>
+      <Route path='/User' element={<Navigate to="/user" replace />} />
       <Route path='*' element={<Notfound/>}/>
     </Routes>
     
